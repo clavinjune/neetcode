9c039947a1f906a765db6195492615c41406c45a
clavinjune <24659468+clavinjune@users.noreply.github.com>
2025-09-07 18:13:17 +0700

    back to the most readable one, since the optimization is no significant
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- Arrays & Hashing/217. Contains Duplicate/main.go
+++ Arrays & Hashing/217. Contains Duplicate/main.go
@@ -1,32 +1,13 @@
 package main
 
 func containsDuplicate(nums []int) bool {
-	n := len(nums)
-
-	if n <= 1 {
-		return false
-	}
-
 	m := make(map[int]struct{})
 	v := struct{}{}
-
-	for i, j := 0, n-1; i <= j; i, j = i+1, j-1 {
-		ni := nums[i]
-		if _, ok := m[ni]; ok {
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
 			return true
 		}
-		m[ni] = v
-
-		if i == j {
-			break
-		}
-
-		nj := nums[j]
-		if _, ok := m[nj]; ok {
-			return true
-		}
-		m[nj] = v
+		m[n] = v
 	}
-
 	return false
 }
