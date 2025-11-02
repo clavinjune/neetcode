2f7d502a7aea3f6d64d7eea6b75e52caebd29312
clavinjune <24659468+clavinjune@users.noreply.github.com>
2025-11-03 00:55:51 +0700

    use two pointers
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- 2. Two Pointers/167. Two Sum II - Input Array Is Sorted/main.go
+++ 2. Two Pointers/167. Two Sum II - Input Array Is Sorted/main.go
@@ -1,26 +1,16 @@
 package main
 
 func twoSum(numbers []int, target int) []int {
-	m := make(map[int]int, len(numbers))
-	for _, n := range numbers {
-		m[n] = target - n
-	}
+	i, j := 0, len(numbers)-1
 
-	var firstIdx, secondNumber int
-	for i, n := range numbers {
-		secondNumber = m[n]
-		if _, ok := m[secondNumber]; ok {
-			firstIdx = i + 1
-			break
-		}
-	}
-
-	for i, n := range numbers {
-		if i <= firstIdx-1 {
-			continue
-		}
-		if n == secondNumber {
-			return []int{firstIdx, i + 1}
+	for i < j {
+		sum := numbers[i] + numbers[j]
+		if sum > target {
+			j--
+		} else if sum < target {
+			i++
+		} else if sum == target {
+			return []int{i + 1, j + 1}
 		}
 	}
 
